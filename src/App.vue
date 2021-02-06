574abf7f5a64ad5d4a9b239e4fb6a0bfa697dd8b
Your Name <you@example.com>
2021-02-07 05:27:01 +0900

    Vuejsのv-forディレクティブの学習記録として残します

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+  <h1>{{ message }}</h1>
+    <p>ulタグを場合</p>
+    <ul>
+      <li v-for="num in numbers" :key="num">{{ num }}</li>
+    </ul>
+    <p>olタグを場合</p>
+    <ol>
+      <li v-for="boy in boys" :key="boy">{{ boy }}</li>
+    </ol>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  data: function() {
+    return {
+      message: "v-forを使った条件付きレンダリングのテスト",
+      numbers :[1,2,3,4,5,6,7,8,9,10],
+      boys : ["太郎","博","孝介","昭雄"]
+    }
+  }
+}
+</script>
+
+
